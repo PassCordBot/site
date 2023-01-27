bb018a3845de92ff0251ac0376544476ea799084
AngelNext <contact@angelnext.dev>
2023-01-27 21:36:29 +0100

    temp commit

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,12 +1,12 @@
+import type { ReactElement } from 'react';
 import Fade from 'react-reveal/Fade';
 
-const Text = () => {
+const Text = ({ children }: { children: ReactElement | ReactElement[] | string }) => {
 	return (
 		<Fade bottom>
 			<div className='md:mt-80 mt-56 grid place-items-center'>
 				<h1 className='grid ml-14 font-unisans gap-8 text-6xl font-[1000] text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 to-indigo-900'>
-					<span>Know your members.</span>
-					<span>Know your community.</span>
+					{children}
 				</h1>
 			</div>
 		</Fade>
